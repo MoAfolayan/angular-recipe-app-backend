78db14609649b78871cab8e34892d4a61eb5a062
Mo Afolayan <mo.afolayan@gmail.com>
2021-09-03 10:28:52 -0500

    Update UserController.cs

--- Recipe.Sonar/Controllers/UserController.cs
+++ Recipe.Sonar/Controllers/UserController.cs
@@ -14,7 +14,6 @@ namespace Recipe.Sonar.Controllers
     public class UserController : ControllerBase
     {
         private readonly ILogger<UserController> _logger;
-
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
